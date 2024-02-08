511552496e287c4095a3bf4c0eb9c2c9a6eb576f
mostafa <mostafameerzad@gmail.com>
2024-02-08 18:38:02 +0430

    but to gether a working multi-step form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,53 @@
+import { FormEvent, useState } from "react";
+import AccountForm from "./AccountForm";
+import AddressForm from "./AddressForm";
+import UserForm from "./UserForm";
 import { useMultiStepForm } from "./useMultiStepForm";
 
+type FormData = {
+  firstName: string;
+  lastName: string;
+  age: string;
+  street: string;
+  city: string;
+  state: string;
+  zip: string;
+  email: string;
+  password: string;
+};
+const INITIAL_DATA: FormData = {
+  firstName: "",
+  lastName: "",
+  age: "",
+  street: "",
+  city: "",
+  state: "",
+  zip: "",
+  email: "",
+  password: "",
+};
+
 const App = () => {
+  const [data, setData] = useState(INITIAL_DATA);
+  const updateFields = (fields: Partial<FormData>) => {
+    setData((prev) => ({ ...prev, ...fields }));
+  };
   const { currentStepIndex, step, steps, next, back, isFirstStep, isLastStep } =
-    useMultiStepForm([<div>One</div>, <div>Two</div>]);
+    useMultiStepForm([
+      <UserForm {...data} updateFields={updateFields} />,
+      <AddressForm {...data} updateFields={updateFields} />,
+      <AccountForm {...data} updateFields={updateFields} />,
+    ]);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if(!isLastStep) return next()
+
+    alert("Successful Account Creation")
+  };
   return (
     <div className="main">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="index">
           {currentStepIndex + 1}/{steps.length}
         </div>
@@ -19,9 +61,7 @@ const App = () => {
             </button>
           )}
 
-          <button type="button" onClick={next}>
-            {isLastStep ? "Finish" : "Next"}
-          </button>
+          <button type="submit">{isLastStep ? "Finish" : "Next"}</button>
         </div>
       </form>
     </div>
